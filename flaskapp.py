9085f9d18d013c00ab82dec846422f7be06011b4
Ayoub Afdel <ayafdel@s2r3p1.1337.ma>
2021-06-25 16:46:41 +0100

    flask test

---
+++ flaskapp.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+from wtforms import Form, TextAreaField, validators
+import pickle
+import sqlite3
+import os
+import numpy as np
+from sklearn.externals import joblib
+loaded_model=joblib.load("./model/model.pkl")
+loaded_stop=joblib.load("./model/stopwords.pkl")
+loaded_vec=joblib.load("./model/vectorizer.pkl")
+app = Flask(__name__)
+def classify(document):
+ label = {0: 'negative', 1: 'positive'}
+ X = loaded_vec.transform([document])
+ y = loaded_model.predict(X)[0]
+ proba = np.max(loaded_model.predict_proba(X))
+ return label[y], proba
+class ReviewForm(Form):
+ moviereview = TextAreaField('',[validators.DataRequired(),validators.length(min=15)])
+@app.route('/')
+def index():
+ form = ReviewForm(request.form)
+ return render_template('reviewform.html', form=form)
+@app.route('/results', methods=['POST'])
+def results():
+ form = ReviewForm(request.form)
+ if request.method == 'POST' and form.validate():
+  review = request.form['moviereview']
+  y, proba = classify(review)
+ return render_template('results.html',content=review,prediction=y,probability=round(proba*100, 2))
+ return render_template('reviewform.html', form=form)
+if __name__ == '__main__':
+    app.jinja_env.auto_reload = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.run(debug=True, host='0.0.0.0')
+
