cb05b43bf1b6d2dcb3a229ab139c538704a96c20
Ayoub Afdel <ayafdel@s2r3p1.1337.ma>
2021-07-01 10:39:35 +0100

    added db still error except

--- flaskapp.py
+++ flaskapp.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from wtforms import Form, TextAreaField, validators
+from datetime import datetime
 import pickle
 import sqlite3
 import os
@@ -15,7 +16,8 @@ db = SQLAlchemy(app)
 class Movies(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(200), nullable = False)
-    categorie = db.Column(db.String(200),nullable = False)
+    #categorie = db.Column(db.String(200),nullable = False)
+    date_created = db.Column(db.DateTime,default= datetime.utcnow)
 def __repr__(self):
     return '<Name %r>' % self.id
 def classify(document):
@@ -26,6 +28,21 @@ def classify(document):
  return label[y], proba
 class ReviewForm(Form):
  moviereview = TextAreaField('',[validators.DataRequired(),validators.length(min=15)])
+@app.route('/add_movie', methods=['POST', 'GET'])
+def add_movie():
+    if request.method == "POST":
+        movie_name = request.form['name']
+        new_movie = Movies(name=movie_name)
+        try:
+            db.session.add(new_movie)
+            db.session.commit()
+            return redirect('/add_movie.html')
+        except:
+            return "ERROR LOL"
+    else:
+        movies = Movies.query.order_by(Movies.date_created)
+        return render_template("add_movie.html", )
+    return render_template("add_movie.html")
 @app.route('/review')
 def index():
  form = ReviewForm(request.form)
